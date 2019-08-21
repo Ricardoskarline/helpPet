7787929a1323675fe61a23309e89d935ff706f82
Ricardo Santos <ricardo_skarline@hotmail.com>
2019-08-20 22:17:12 -0300

    Heroku apresentou erro, alteracao linha import

--- helpPet/settings.py
+++ helpPet/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -127,5 +128,5 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-import django_heroku
+
 django_heroku.settings(locals())
